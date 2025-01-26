32ac1a35b9fdfe2633dd29b444ba2a8b00f05dbd
Verdi Kapuku <st0mxhack@gmail.com>
2025-01-27 00:39:29 +0100

    Add Pinecone module
    Easier to work with than Svector. They have an UI I can browse. Namespaces for segmenting per customer, etc...

--- code_sushi/vector/__init__.py
+++ code_sushi/vector/__init__.py
@@ -1,5 +1,6 @@
 from .voyage_embed import VoyageEmbed
 from .svector import SVector
 from .vector_record import VectorRecord
+from .pinecone import Pinecone
 
-__all__ = ["VoyageEmbed", "SVector", "VectorRecord"]
+__all__ = ["VoyageEmbed", "SVector", "VectorRecord", "Pinecone"]
---
+++ code_sushi/vector/pinecone.py
@@ -0,0 +1,44 @@
+from pinecone import Pinecone as PineconeClient
+from typing import Optional, List
+from .vector_record import VectorRecord
+from .utils import chunks
+import os
+
+class Pinecone:
+    """
+    Responsible for interacting with the Pinecone API for managing indexes and vector DB entries.
+    """
+    def __init__(self, index_name: Optional[str] = None, pool_threads: int = 30):
+        api_key = os.getenv("SUSHI_PINECONE_API_KEY")
+        self.client = PineconeClient(api_key=api_key, pool_threads=pool_threads)
+
+        if not index_name:
+            index_name = os.getenv("SUSHI_PINECONE_INDEX_NAME")
+        
+        self.index = self.client.Index(index_name, pool_threads=pool_threads)
+    
+    def write_many(self, records: List[VectorRecord], chunk_size: int = 400):
+        """
+        Write many records at once to the Pinecone index.
+        """
+        items = []
+        for record in records:
+            items.append({
+                "id": record.key,
+                "values": record.embedding,
+                "metadata": record.metadata
+            })
+
+        with self.index as index:
+            # Send chunked requests in parallel
+            async_results = [
+                index.upsert(vectors=ids_vectors_chunk, async_req=True)
+                for ids_vectors_chunk in chunks(items, chunk_size)
+            ]
+
+            # Wait for and retrieve responses (this raises in case of error)
+            res = [async_result.get() for async_result in async_results]
+            return len(res)
+
+
+    
