978356882c8bf826ff0625ca0e38cd0492791da7
Verdi Kapuku <st0mxhack@gmail.com>
2025-01-26 01:08:20 +0100

    Add try/catch block
    Better error handling. Otherwise it doesnt bubble up in the Terminal

--- code_sushi/agents/agent.py
+++ code_sushi/agents/agent.py
@@ -1,9 +1,9 @@
 from typing import List
 from code_sushi.context import Context, LogLevel
-from code_sushi.core import write_summary_to_file
-from .llm_client import summarize_file
+from code_sushi.core import write_summary_to_file, File
 from code_sushi.jobs import JobTask, TaskStatus
 from code_sushi.itamae import Itamae
+from .llm_client import summarize_file
 import time
 
 class Agent:
@@ -24,24 +24,40 @@ class Agent:
         if self.context.log_level.value >= LogLevel.DEBUG.value:
             print(f"Processing {task.name}...")
 
-        self.busy = True
-        self.tasks_completed += 1
-        task.update_status(TaskStatus.IN_PROGRESS)
+        try:
+            tasks = []
+            self.busy = True
+            self.tasks_completed += 1
+            task.update_status(TaskStatus.IN_PROGRESS)
 
-        # 1- Summarize the file content using LLM
-        content = open(task.file.absolute_path).read()
-        summary = summarize_file(self.context, task.file.clean_path, content)
-        write_summary_to_file(self.context, task.file, summary)
+            # 1- Summarize the file content using LLM
+            origin_file = task.absolute_path()
+            content = open(origin_file).read()
+            summary = summarize_file(self.context, task.relative_path(), content)
+            write_summary_to_file(self.context, task.file, summary)
 
-        # 2- Extract logical chunks from the file
-        chunks = Itamae.slice_chunks(self.context, task.file)
-        # TODO: Add to queue for further summarization of the chunk
+            if task.is_file():
+                # 2- Extract logical chunks from the file
+                chunks = Itamae().slice_chunks(self.context, task.file)
+                for chunk in chunks:
+                    # Create a new task for each chunk
+                    temp_file = File(self.context.repo_path, chunk.absolute_path)
+                    temp_file.absolute_path = chunk.absolute_path
+                    temp_file.clean_path = chunk.relative_path
 
-        task.update_status(TaskStatus.COMPLETE)
-        self.busy = False
+                    task = JobTask(self.context, chunk=chunk, file=temp_file)
+                    tasks.append(task)
 
-        if self.context.log_level.value >= LogLevel.DEBUG.value:
-            runtime = time.time() - start_time
-            print(f"Agent [{self.id}] Completed Job {task.name} in {runtime:.2f} seconds.")#
+            task.update_status(TaskStatus.COMPLETE)
+            self.busy = False
 
-        return []
+            if self.context.log_level.value >= LogLevel.DEBUG.value:
+                runtime = time.time() - start_time
+                print(f"Agent [{self.id}] Completed Job {task.name} in {runtime:.2f} seconds.")#
+
+            return tasks
+        except Exception as e:
+            task.update_status(TaskStatus.FAILED)
+            self.busy = False
+            print(f"Agent [{self.id}] Failed Job {task.name}. Error: {e}")
+            return []
