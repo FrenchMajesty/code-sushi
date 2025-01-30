cfa99c8c83a8aca0355bfe02e7dfed8fe89babb0
Verdi Kapuku <st0mxhack@gmail.com>
2025-01-30 22:03:04 +0100

    Create TreeProcessor class
    Makes the process of parsing a file into function chunks more organized

--- code_sushi/itamae/itamae.py
+++ code_sushi/itamae/itamae.py
@@ -1,10 +1,10 @@
-import os
 from tree_sitter import Parser, Tree
-from .utils import extract_functions, init_parser, save_raw_function
+from .utils import save_raw_function
 from .logical_chunk import LogicalChunk
+from .tree_processor import TreeProcessor
 from code_sushi.core import File
 from code_sushi.context import Context, LogLevel
-from typing import Optional, List
+from typing import List
 
 class Itamae:
     """
@@ -21,27 +21,26 @@ class Itamae:
     def slice_chunks(self, context: Context, file: File) -> List[LogicalChunk]:
         """Process the file to and extract every individual function."""
         try:
-            if context.log_level.value >= LogLevel.VERBOSE.value:
+            if context.is_log_level(LogLevel.VERBOSE):
                 print(f"Slicing chunks for file: {file.relative_path}")
 
-            parser = init_parser(context, file.ext)
-            if not parser:
+            parser = TreeProcessor(context, file)
+            if not parser.is_supported():
                 return []
 
-            code, syntax_tree = self.parse_content(parser, file.absolute_path)
+            functions = parser.extract()
 
-            functions = extract_functions(code, syntax_tree)
-            if context.log_level.value >= LogLevel.VERBOSE.value:
+            if context.is_log_level(LogLevel.VERBOSE):
                 print(f"Extracted {len(functions)} functions/methods from {file.relative_path}")
 
-            # Parse each function into a logical chunk object
+            # Parse each function into a logical chunk object TODO: Remake in the age of git chunks
             chunks = []
             for func in functions:
                 output_dir = context.output_dir + "/" + file.relative_path + ".functions/"
                 output_dir = output_dir.replace("//", "/")
                 file_path = save_raw_function(func, output_dir, file.ext)
 
-                if context.log_level.value >= LogLevel.VERBOSE.value:
+                if context.is_log_level(LogLevel.VERBOSE):
                     print(f"Saved function: {file_path}")
 
                 chunks.append(LogicalChunk(context, file, func["name"], func["code"], file_path))
@@ -50,14 +49,3 @@ class Itamae:
         except Exception as e:
             print(f"Error slicing chunks: {e}")
             return []
-
-    def parse_content(self, parser: Parser, file_path: str) -> tuple[str, Tree]:
-        """Parse the content of the file and return the syntax tree."""
-
-        try:
-            code = open(file_path, "r", encoding="utf8").read()
-            tree = parser.parse(bytes(code, "utf8"))
-            return code, tree
-        except Exception as e:
-            print(f"Error parsing content: {e}")
-            raise e
---
+++ code_sushi/itamae/tree_processor.py
@@ -0,0 +1,86 @@
+from code_sushi.core import File
+from code_sushi.context import Context, LogLevel
+from typing import List, Optional
+from tree_sitter import Parser, Language, Tree
+import tree_sitter_typescript as tstypescript
+import tree_sitter_javascript as tsjavascript
+import tree_sitter_php as tsphp
+import tree_sitter_python as tspython
+import hashlib
+
+LANGUAGES = {
+    ".ts": Language(tstypescript.language_typescript()),
+    ".tsx": Language(tstypescript.language_tsx()),
+    ".js": Language(tsjavascript.language()),
+    ".py": Language(tspython.language()),
+    ".php": Language(tsphp.language_php()),
+    # Add more languages here if needed
+}
+
+
+class TreeProcessor:
+    def __init__(self, context: Context, file: File):
+        self.context = context
+        self.file = file
+
+        self.parser = self._init_parser()
+
+    def _init_parser(self) -> Optional[Parser]:
+        """
+        Initialize the parser for the given file. Return None if the extension is not supported.
+        """
+        try:
+            language = LANGUAGES.get(self.file.ext, None)
+            if not language:
+                if self.context.is_log_level(LogLevel.DEBUG):
+                    print(f"Itamae cannot slice unsupported file: {self.file.ext}. Skipping...")
+                return None
+
+            return Parser(language)
+        except Exception as e:
+            print(f"Error in TreeProcessor.init_parser(): {e}")
+            return None
+
+    def is_supported(self) -> bool:
+        """
+        Check if the file can be processed by this processor.
+        """
+        return self.parser is not None
+
+    def extract(self) -> List[LogicalChunk]:
+        """
+        Parse the content of the file and return a list of LogicalChunks.
+        """
+        code, syntax_tree = self.parse_content(parser, file.absolute_path)
+        functions = self._extract_functions(code, syntax_tree)
+        return functions
+
+    def _extract_functions(self, code: str, tree: Tree) -> List[Function]:
+        """
+        Extract all function/method definitions in the code.
+        """
+        root_node = tree.root_node
+        functions = []
+
+        # Traverse the syntax tree for function definitions
+        def traverse(node):
+            valid_types = set(["function_declaration", "method_definition", "method_declaration"])
+            if node.type in valid_types:
+                func_name_node = node.child_by_field_name("name")
+                func_name = func_name_node.text.decode("utf8") if func_name_node else "anonymous"
+                start_line, _ = node.start_point
+                end_line, _ = node.end_point
+
+                if func_name == "anonymous":
+                    random_hash = hashlib.sha256(code.encode()).hexdigest()[:6]
+                    func_name = f"anonymous_{random_hash}"
+
+                # Extract function code
+                func_code = "\n".join(code.splitlines()[start_line:end_line + 1])
+                functions.append({"name": func_name, "code": func_code})
+            
+            for child in node.children:
+                traverse(child)
+
+        traverse(root_node)
+        return functions
