e230160fd657bbcafec38d439598e3c9cc5591c7
Verdi Kapuku <st0mxhack@gmail.com>
2025-02-02 01:07:51 +0100

    Increase package version
    Preparing for the next release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="code-sushi",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[],  # Add dependencies if needed
     entry_points={
